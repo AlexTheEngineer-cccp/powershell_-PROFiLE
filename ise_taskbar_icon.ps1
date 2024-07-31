d7bdbed9a7b9a662ed6fdc5643ac28ccf7b60a6e
AlexTheEngineer <45663192+AlexTheEngineer-cccp@users.noreply.github.com>
2024-07-31 12:11:11 +0300

    Update ise_taskbar_icon.ps1

--- ise_taskbar_icon.ps1
+++ ise_taskbar_icon.ps1
@@ -10,17 +10,23 @@
   <CustomTaskbarLayoutCollection> 
     <defaultlayout:TaskbarLayout> 
       <taskbar:TaskbarPinList> 
-          <taskbar:DesktopApp DesktopApplicationLinkPath="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell ISE.lnk"/> 
+          <taskbar:DesktopApp DesktopApplicationLinkPath="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell ISE.lnk"/> 
       </taskbar:TaskbarPinList> 
     </defaultlayout:TaskbarLayout> 
   </CustomTaskbarLayoutCollection> 
 </LayoutModificationTemplate> 
 '@ | 
-Out-File -FilePath "$env:TEMP\taskbar.iSE.xml" -Encoding UTF8 -Force 
+Out-File -FilePath "$env:TEMP\taskbar.iSE.xml" -Encoding utf8 -Force 
 Import-StartLayout "$env:TEMP\taskbar.iSE.xml" -MountPath c:\  
 rm "$env:TEMP\taskbar.iSE.xml"
 
- [System.IO.FileInfo] $LinkFile = "$($env:APPDATA)\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell ISE.lnk"
+#[System.IO.FileInfo] $LinkFile = "$($env:APPDATA)\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell ISE.lnk"
+[System.IO.FileInfo] $LinkFile = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell ISE.lnk"
+
+
+# Test-Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell ISE.lnk"
+
+
 
 $tempFileName = [IO.Path]::GetRandomFileName()
 $tempFile = [IO.FileInfo][IO.Path]::Combine($LinkFile.Directory, $tempFileName)
@@ -31,6 +37,7 @@ $reader = $LinkFile.OpenRead()
 while ($reader.Position -lt $reader.Length)
 {        
     $byte = $reader.ReadByte()
+    # "$($reader.Position) `t $byte"
     if ($reader.Position -eq 22) {
         $byte = 34
     }
